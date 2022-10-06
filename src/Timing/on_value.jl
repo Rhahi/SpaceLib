6e6d6d6c6bcebee9201b2e5ee54e661dcb2fdf9b
Rhahi <git@rhahi.com>
2022-10-06 22:04:49 +0200

    Add progress logging to bedrock altitude delay

--- src/Timing/on_value.jl
+++ src/Timing/on_value.jl
@@ -26,3 +26,33 @@ function delay_on_bedrock_altitude(sp::Spacecraft, target_alt::Real, timeout::Re
     end
     h₀, h₁
 end
+
+
+function delay_on_bedrock_altitude(sp::Spacecraft, target_alt::Real, timeout::Real, name::String)
+    acquire(sp, :stream)
+    ecef = ReferenceFrame.BCBF(sp)
+    flight = SpaceLib.Telemetry.get_flight(sp, ecef)
+    t₀, h₀, t₁, h₁ = missing, missing, missing, missing
+    telemetry_stream(sp, (SC.get_UT(), SC.Flight_get_BedrockAltitude(flight))) do stream
+        release(sp, :stream)
+        t₀, h₀ = next(stream)
+        @withprogress name=name begin
+            for (now, alt,) in stream
+                t₁ = now
+                h₁ = alt
+                if h₀ > target_alt
+                    @logprogress min(1, (h₀-alt) / target_alt)
+                    alt ≤ target_alt && break
+                else
+                    @logprogress min(1, (alt-h₀) / target_alt)
+                    alt ≥ target_alt && break
+                end
+                (now - t₀) ≥ (timeout) && break
+                yield()
+            end
+        end
+        @tracev 2 "delay complete" duration=t₁-t₀ altitude=h₁
+    end
+    h₀, h₁
+end
+
