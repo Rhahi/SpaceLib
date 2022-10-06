e74dd4bea0f7bf0eea65ca048bf1a7cdf68b5816
Rhahi <git@rhahi.com>
2022-10-06 22:35:05 +0200

    add semaphore for stage

--- src/Control/staging.jl
+++ src/Control/staging.jl
@@ -7,7 +7,9 @@ import KRPC.Interface.SpaceCenter.Helpers as RC
 function stage(sp::Spacecraft)
     @trace "Stage"
     acquire(sp, :stage)
-    RC.ActivateNextStage(RC.Control(sp.ves))
+    acquire(sp, :stream) do
+        RC.ActivateNextStage(RC.Control(sp.ves))
+    end
     @async begin
         delay(sp, 0.5625)  # Respect KSP "stagingCooldownTimer"
         release(sp, :stage)
