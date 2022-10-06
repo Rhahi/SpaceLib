ba7a2944660c64a5420bed38be2bdb0207db4bd8
Rhahi <git@rhahi.com>
2022-10-06 22:32:37 +0200

    add semaphore for throttle!

--- src/Control/throttle.jl
+++ src/Control/throttle.jl
@@ -5,7 +5,9 @@ import KRPC.Interface.SpaceCenter.Helpers as RC
 "Set main pilot throttle of the spacecraft."
 function throttle!(sp::Spacecraft, value::Real)
     @tracev 1 "set throttle" th=value
-    control = RC.Control(sp.ves)
-    target_value = clamp(value, 0., 1.)
-    RC.Throttle!(control, convert(Float32, target_value))
+    acquire(sp, :stream) do
+        control = RC.Control(sp.ves)
+        target_value = clamp(value, 0., 1.)
+        RC.Throttle!(control, convert(Float32, target_value))
+    end
 end
