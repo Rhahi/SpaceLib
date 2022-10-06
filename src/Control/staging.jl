084d8d95dc07f0f5841769995a4b021e6570528d
Rhahi <git@rhahi.com>
2022-10-06 22:41:04 +0200

    add documentation for stage!

--- src/Control/staging.jl
+++ src/Control/staging.jl
@@ -4,6 +4,15 @@ using LoggingExtras
 import KRPC.Interface.SpaceCenter.Helpers as RC
 
 
+"""
+    stage!(sp)
+
+Stage the spacecraft.
+
+Wait 0.5625 seconds so that next stage! will work.
+Because this acquires :stream for activating next stage, if you somehow need to
+stage in the middle of acquired :stream, directly call ActivateNextStage.
+"""
 function stage!(sp::Spacecraft)
     @trace "Stage"
     acquire(sp, :stage)
@@ -11,7 +20,8 @@ function stage!(sp::Spacecraft)
         RC.ActivateNextStage(RC.Control(sp.ves))
     end
     @async begin
-        delay(sp, 0.5625)  # Respect KSP "stagingCooldownTimer"
+        delay(sp, 0.5625)
+        @tracev 2 "Stage is ready again"
         release(sp, :stage)
     end
 end
