a83d194be4c950f0759a45a6e9a9000d3ab73267
Rhahi <git@rhahi.com>
2022-10-06 22:28:59 +0200

    Reduce throttle! by using Real over Float/Int

--- src/Control/throttle.jl
+++ src/Control/throttle.jl
@@ -2,17 +2,9 @@ using SpaceLib
 import KRPC.Interface.SpaceCenter.Helpers as RC
 
 
-function throttle!(sp::Spacecraft, value::Float64)
+function throttle!(sp::Spacecraft, value::Real)
     @tracev 1 "set throttle" th=value
     control = RC.Control(sp.ves)
     target_value = clamp(value, 0., 1.)
-    RC.Throttle!(control, convert(Float32, target_value))
-end
-
-
-function throttle!(sp::Spacecraft, value::Int64)
-    @tracev 1 "set throttle" th=value
-    control = RC.Control(sp.ves)
-    target_value = clamp(value, 0, 1)
     RC.Throttle!(control, convert(Float32, target_value))
 end
