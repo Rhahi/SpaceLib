47a0dd8a6b852431a4084cefaa19320b5ebfcdcb
Rhahi <git@rhahi.com>
2022-10-16 23:36:28 +0200

    refactor bedrock altitude delay

--- src/Timing/on_value.jl
+++ src/Timing/on_value.jl
@@ -1,58 +1,49 @@
-import KRPC.Interface.SpaceCenter as SC
-using SpaceLib
-using SpaceLib.Timing, SpaceLib.Telemetry, SpaceLib.Navigation
-
-
-function delay_on_bedrock_altitude(sp::Spacecraft, target_alt::Real, timeout::Real)
+function setup__bedrock_altitude(sp::Spacecraft)
     acquire(sp, :stream)
     ecef = ReferenceFrame.BCBF(sp)
     flight = SpaceLib.Telemetry.get_flight(sp, ecef)
-    t₀, h₀, t₁, h₁ = missing, missing, missing, missing
-    telemetry_stream(sp, (SC.get_UT(), SC.Flight_get_BedrockAltitude(flight))) do stream
-        release(sp, :stream)
-        t₀, h₀ = next(stream)
-        for (now, alt,) in stream
-            t₁ = now
+    listener = add_stream(sp.conn, (SC.Flight_get_BedrockAltitude(flight),))
+    release(sp, :stream)
+    value::Float64, = next(listener)
+    listener, sp.system.ut, missing, value, missing
+end
+
+
+function delay__bedrock_altitude(sp::Spacecraft; target::Real, timeout::Real=-1)
+    listener, t₀, t₁, h₀, h₁ = setup__bedrock_altitude(sp)
+    for (alt,) in listener
+        t₁ = sp.system.ut
+        h₁ = alt
+        if h₀ > target
+            alt ≤ target && break
+        else
+            alt ≥ target && break
+        end
+        timeout > 0 && (t₁ - t₀) ≥ (timeout) && break
+        yield()
+    end
+    @tracev 2 "delay__bedrock_altitude complete" duration=t₁-t₀ altitude=h₁
+    h₀, h₁
+end
+
+
+function delay__bedrock_altitude(sp::Spacecraft, label::String; target::Real, timeout::Real=-1)
+    listener, t₀, t₁, h₀, h₁ = setup__bedrock_altitude(sp)
+    @withprogress name=label begin
+        for (alt,) in listener
+            t₁ = sp.system.ut
             h₁ = alt
-            if h₀ > target_alt
-                alt ≤ target_alt && break
+            if h₀ > target
+                @logprogress label min(1, (h₀-alt) / target) _group=:pgbar
+                alt ≤ target && break
             else
-                alt ≥ target_alt && break
+                @logprogress label min(1, (alt-h₀) / target) _group=:pgbar
+                alt ≥ target && break
             end
-            (now - t₀) ≥ (timeout) && break
+            timeout > 0 && t₁ - t₀ ≥ timeout && break
             yield()
         end
-        @tracev 2 "delay complete" duration=t₁-t₀ altitude=h₁
     end
+    @tracev 2 "delay__bedrock_altitude complete" duration=t₁-t₀ altitude=h₁
     h₀, h₁
 end
-
-
-function delay_on_bedrock_altitude(sp::Spacecraft, target_alt::Real, timeout::Real, name::String)
-    acquire(sp, :stream)
-    ecef = ReferenceFrame.BCBF(sp)
-    flight = SpaceLib.Telemetry.get_flight(sp, ecef)
-    t₀, h₀, t₁, h₁ = missing, missing, missing, missing
-    telemetry_stream(sp, (SC.get_UT(), SC.Flight_get_BedrockAltitude(flight))) do stream
-        release(sp, :stream)
-        t₀, h₀ = next(stream)
-        @withprogress name=name begin
-            for (now, alt,) in stream
-                t₁ = now
-                h₁ = alt
-                if h₀ > target_alt
-                    @logprogress min(1, (h₀-alt) / target_alt)
-                    alt ≤ target_alt && break
-                else
-                    @logprogress min(1, (alt-h₀) / target_alt)
-                    alt ≥ target_alt && break
-                end
-                (now - t₀) ≥ (timeout) && break
-                yield()
-            end
-        end
-        @tracev 2 "delay complete" duration=t₁-t₀ altitude=h₁
-    end
-    h₀, h₁
-end
-
--- test/Timing/test_value.jl
+++ test/Timing/test_value.jl
@@ -5,19 +5,19 @@ import KRPC.Interface.SpaceCenter as SC
 @testset "Altitude-time test" begin
     connect_to_spacecraft() do sp
         @testset "Warmup" begin
-            w3 = @async SpaceLib.Timing.delay(sp, 2.)
+            w3 = @async Timing.delay(sp, 2.)
             @test typeof(fetch(w3)) <: Tuple
         end
 
         @testset "Async test" begin
-            w1 = @async SpaceLib.Timing.delay_on_bedrock_altitude(sp, 100., 5.)
-            w2 = @async SpaceLib.Timing.delay_on_bedrock_altitude(sp, 100., 4.)
-            w3 = @async SpaceLib.Timing.delay(sp, 2.)
-            @async SpaceLib.Timing.delay_on_bedrock_altitude(sp, 100., 4.)
-            @async SpaceLib.Timing.delay_on_bedrock_altitude(sp, 100., 4.)
-            @async SpaceLib.Timing.delay_on_bedrock_altitude(sp, 100., 1.)
-            @async SpaceLib.Timing.delay_on_bedrock_altitude(sp, 100., 4.)
-            @async SpaceLib.Timing.delay_on_bedrock_altitude(sp, 100., 4.)
+            w1 = @async Timing.delay__bedrock_altitude(sp, target=100., timeout=5.)
+            w2 = @async Timing.delay__bedrock_altitude(sp, target=100., timeout=4.)
+            w3 = @async Timing.delay(sp, 2.)
+            @async Timing.delay__bedrock_altitude(sp, target=100., timeout=4.)
+            @async Timing.delay__bedrock_altitude(sp, target=100., timeout=4.)
+            @async Timing.delay__bedrock_altitude(sp, target=100., timeout=1.)
+            @async Timing.delay__bedrock_altitude(sp, target=100., timeout=4.)
+            @async Timing.delay__bedrock_altitude(sp, target=100., timeout=4.)
             @test typeof(fetch(w1)) <: Tuple
             @test typeof(fetch(w2)) <: Tuple
         end
