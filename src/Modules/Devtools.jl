21d0630149e90bd76fabf94e84f198888d71a00f
Rhahi <git@rhahi.com>
2022-10-17 22:16:37 +0200

    add devtool for modules

---
+++ src/Modules/Devtools.jl
@@ -0,0 +1,54 @@
+module DevTools
+
+import KRPC.Interface.SpaceCenter.Helpers as SCH
+import KRPC.Interface.SpaceCenter.RemoteTypes as SCR
+
+function list_actions(part::SCR.Part)
+    modules = SCH.Modules(part)
+    println("--- actions ---")
+    for m ∈ modules
+        actions = SCH.Actions(m)
+        println("Module[$(length(actions))]: ", SCH.Name(m))
+        for a in actions
+            println("  -> ", a)
+        end
+    end
+end
+
+
+function list_events(part::SCR.Part)
+    modules = SCH.Modules(part)
+    println("--- events ---")
+    for m ∈ modules
+        events = SCH.Events(m)
+        println("Module[$(length(events))]: ", SCH.Name(m))
+        for e in events
+            println("  -> ", e)
+        end
+    end
+end
+
+
+function list_fields(part::SCR.Part)
+    modules = SCH.Modules(part)
+    println("--- fields ---")
+    for m ∈ modules
+        fields = SCH.Fields(m)
+        println("Module[$(length(fields))]: ", SCH.Name(m))
+        for f in fields
+            println("  -> ", f)
+        end
+    end
+end
+
+
+function list_info(part::SCR.Part)
+    println(SCH.Title(part))
+    list_actions(part)
+    println("")
+    list_events(part)
+    println("")
+    list_fields(part)
+end
+
+end #module
--- src/Modules/Modules.jl
+++ src/Modules/Modules.jl
@@ -4,6 +4,7 @@ import KRPC.Interface.SpaceCenter.Helpers as SCH
 import KRPC.Interface.SpaceCenter.RemoteTypes as SCR
 
 include("Core.jl")
+include("devtools.jl")
 
 export Core
 
