049087b03b278dbb79a2ffd8a8630bbe4dbba295
Rhahi <git@rhahi.com>
2022-10-17 22:08:59 +0200

    make test_value more challenging

--- test/Timing/test_value.jl
+++ test/Timing/test_value.jl
@@ -12,14 +12,18 @@ import KRPC.Interface.SpaceCenter as SC
         @testset "Async test" begin
             w1 = @async Timing.delay__bedrock_altitude(sp, target=100., timeout=5.)
             w2 = @async Timing.delay__bedrock_altitude(sp, target=100., timeout=4.)
-            w3 = @async Timing.delay(sp, 2.)
+            @async Timing.delay(sp, 2.)
             @async Timing.delay__bedrock_altitude(sp, target=100., timeout=4.)
             @async Timing.delay__bedrock_altitude(sp, target=100., timeout=4.)
             @async Timing.delay__bedrock_altitude(sp, target=100., timeout=1.)
             @async Timing.delay__bedrock_altitude(sp, target=100., timeout=4.)
             @async Timing.delay__bedrock_altitude(sp, target=100., timeout=4.)
+            @async Timing.delay__bedrock_altitude(sp, target=100., timeout=4.)
+            @async Timing.delay__bedrock_altitude(sp, target=100., timeout=4.)
+            w3 = @async Timing.delay__bedrock_altitude(sp, target=100., timeout=4.)
             @test typeof(fetch(w1)) <: Tuple
             @test typeof(fetch(w2)) <: Tuple
+            @test typeof(fetch(w3)) <: Tuple
         end
     end
 end
