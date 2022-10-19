06b0d3e19b6c919761b7b81732b8a32a17ca5c2c
Rhahi <git@rhahi.com>
2022-10-19 22:58:11 +0200

    make on_value progress bars make more sense

--- src/Timing/on_value.jl
+++ src/Timing/on_value.jl
@@ -2,7 +2,7 @@ function setup__bedrock_altitude(sp::Spacecraft)
     ecef = ReferenceFrame.BCBF(sp)
     flight = SCH.Flight(sp.ves, ecef)
     listener = add_stream(sp.conn, (SC.Flight_get_BedrockAltitude(flight),))
-    value::Float64, = next_value(listener)
+    value::Float64, = KRPC.next_value(listener)
     listener, sp.system.ut, missing, value, missing
 end
 
@@ -28,22 +28,23 @@ end
 
 function delay__bedrock_altitude(sp::Spacecraft, label::String; target::Real, timeout::Real=-1)
     listener, t₀, t₁, h₀, h₁ = setup__bedrock_altitude(sp)
-    @withprogress name=label begin
-        for (alt,) in listener
-            t₁ = sp.system.ut
-            h₁ = alt
-            if h₀ > target
-                @logprogress label min(1, (h₀-alt) / target) _group=:pgbar
-                alt ≤ target && break
-            else
-                @logprogress label min(1, (alt-h₀) / target) _group=:pgbar
-                alt ≥ target && break
+    if target ≠ h₀
+        @withprogress name=label begin
+            for (alt,) in listener
+                t₁ = sp.system.ut
+                h₁ = alt
+                @logprogress label min(1, alt-h₀ / target-h₀) _group=:pgbar
+                if h₀ > target
+                    alt ≤ target && break
+                else
+                    alt ≥ target && break
+                end
+                timeout > 0 && t₁ - t₀ ≥ timeout && break
+                yield()
             end
-            timeout > 0 && t₁ - t₀ ≥ timeout && break
-            yield()
         end
     end
     KRPC.close(listener)
-    @tracev 2 "delay__bedrock_altitude complete" duration=t₁-t₀ altitude=h₁
+    @tracev 2 "delay__bedrock_altitude complete" duration=t₁-t₀ travel=h₁-h₀
     h₀, h₁
 end
