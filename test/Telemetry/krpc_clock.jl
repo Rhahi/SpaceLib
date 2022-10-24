d3f444fa1e177b2cf1684f9e2a89147060f02b5c
Rhahi <git@rhahi.com>
2022-10-24 09:29:24 +0200

    fix test for clocks

--- test/Telemetry/krpc_clock.jl
+++ test/Telemetry/krpc_clock.jl
@@ -5,12 +5,12 @@ using SpaceLib.Telemetry
 @testset "Clock" begin
     connect_to_spacecraft() do sp
         # we did nothing so far, so MET is zero.
-        @test sp.met == 0
-        @test sp.ut > 0
-        ut₀ = sp.ut
+        @test sp.system.met == 0
+        @test sp.system.ut > 0
+        ut₀ = sp.system.ut
 
         # sleep 1 second, and see that time has elapsed around that
         Timing.delay(sp, 1)
-        @test 1 ≤ sp.ut - ut₀ ≤ 2
+        @test 0.9 ≤ sp.system.ut - ut₀ ≤ 2
     end
 end
