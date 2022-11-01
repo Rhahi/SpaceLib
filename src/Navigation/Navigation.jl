078c5fe3611e0dc6f4222c135c5195e165ae88cc
Rhahi <git@rhahi.com>
2022-11-02 00:09:57 +0100

    update Navigation module

--- src/Navigation/Navigation.jl
+++ src/Navigation/Navigation.jl
@@ -1,7 +1,17 @@
 module Navigation
 
+using SpaceLib
+using LinearAlgebra
+import KRPC.Interface.SpaceCenter.RemoteTypes as SCR
+import KRPC.Interface.SpaceCenter.Helpers as SCH
+
 include("referenceframe.jl")
+include("directions.jl")
+include("posvel.jl")
+include("drawing.jl")
 
 export ReferenceFrame
+export directions, up, north, east
+export coordinate, velocity
 
 end
