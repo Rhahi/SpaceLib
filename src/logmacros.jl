007a4e31807723e0c17e9d131a4c8d217a06e7da
Rhahi <git@rhahi.com>
2022-11-02 00:05:16 +0100

    add more log macros

--- src/logmacros.jl
+++ src/logmacros.jl
@@ -18,10 +18,14 @@ end
 
 
 # Debug (-1000)
-const LogTimer     = ExtraLogLevel(-900, "Timer")
-const LogTrace     = ExtraLogLevel(-800, "Trace")
-const LogExit      = ExtraLogLevel(-700, "Exit ")
-const LogEntry     = ExtraLogLevel(-600, "Entry")
+const LogGraphic   = ExtraLogLevel(-900, "Graphic")
+const LogTimer     = ExtraLogLevel(-800, "Timer")
+const LogTraceLoop = ExtraLogLevel(-700, "TraceLoop")
+const LogTrace     = ExtraLogLevel(-600, "Trace")
+const LogExit      = ExtraLogLevel(-400, "Exit ")
+const LogEntry     = ExtraLogLevel(-300, "Entry")
+const LogGuidance  = ExtraLogLevel(-200, "Guidance")
+const LogDev       = ExtraLogLevel(-100, "🐞 Develop")
 # Info (0)
 const LogModule    = ExtraLogLevel( 300, "🟦 Module")
 const LogSystem    = ExtraLogLevel( 400, "🟪 System")
@@ -39,14 +43,29 @@ Base.convert(::Type{Int32}, level::ExtraLogLevel) = level.level
 handle_message(logger::SimpleLogger, level::ExtraLogLevel, args...; kwargs...) = handle_message(logger, convert(LogLevel, level), args...; kwargs...)
 Base.show(io::IO, level::ExtraLogLevel) = print(io, level.name)
 
+"""debug information about drawing"""
+macro log_graphic(exs...)
+    return restore_callsite_source_position!(esc(:($Base.@logmsg LogGraphic $(exs...))), __source__,)
+end
+
 """debug information about timing"""
 macro log_timer(exs...)
     return restore_callsite_source_position!(esc(:($Base.@logmsg LogTimer $(exs...))), __source__,)
 end
 
+"""trace execution history within a function, in a loop"""
+macro log_traceloop(exs...)
+    return restore_callsite_source_position!(esc(:($Base.@logmsg LogTraceLoop $(exs...))), __source__,)
+end
+
 """trace execution history within a function"""
 macro log_trace(exs...)
     return restore_callsite_source_position!(esc(:($Base.@logmsg LogTrace $(exs...))), __source__,)
+end
+
+"""low level log with point of interest. Demote to other levels after debugging."""
+macro log_dev(exs...)
+    return restore_callsite_source_position!(esc(:($Base.@logmsg LogDev $(exs...))), __source__,)
 end
 
 """trace exit of a function, usually for long-running or complex calls"""
@@ -57,6 +76,11 @@ end
 """trace entry of a function"""
 macro log_entry(exs...)
     return restore_callsite_source_position!(esc(:($Base.@logmsg LogEntry $(exs...))), __source__,)
+end
+
+"""guidance logs"""
+macro log_guidance(exs...)
+    return restore_callsite_source_position!(esc(:($Base.@logmsg LogGuidance $(exs...))), __source__,)
 end
 
 """status info from a module"""
