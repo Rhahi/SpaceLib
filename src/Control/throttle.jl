051c67f35f74ff59d6fac7f750db810f0f1352a3
Rhahi <git@rhahi.com>
2022-11-02 01:14:18 +0100

    update throttle name

--- src/Control/throttle.jl
+++ src/Control/throttle.jl
@@ -3,12 +3,11 @@ import KRPC.Interface.SpaceCenter.Helpers as SCH
 
 
 "Set main pilot throttle of the spacecraft."
-function throttle(sp::Spacecraft, value::Real)
-    @log_entry "throttle: $value"
+function throttle!(sp::Spacecraft, value::Real)
+    @log_trace "throttle: $value"
     control = SCH.Control(sp.ves)
     target_value = clamp(value, 0., 1.)
     SCH.Throttle!(control, convert(Float32, target_value))
-    @log_exit "throttle: $value"
 end
 
 
