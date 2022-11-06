7d48a09926a17325a7a3d3ebff501f57e3c5e7cb
Rhahi <git@rhahi.com>
2022-11-06 19:57:29 +0100

    add engine test

--- test/Control/krpc_engines.jl
+++ test/Control/krpc_engines.jl
@@ -1,19 +1,30 @@
 using Test
 using SpaceLib
-using SpaceLib.Control, SpaceLib.Timing
+using SpaceLib.Control
+using SpaceLib.Modules.Engine
+import KRPC.Interface.SpaceCenter.Helpers as SCH
 
-@testset "Throttle" begin
-    connect_to_spacecraft() do sp
-        throttle(sp, 0.5)
-        delay(sp, 1)
+connect_to_spacecraft() do sp
+    @testset "Main throttle control test" begin
+        throttle!(sp, 0.5)
+        Timing.delay(sp, 0.5)
         @test throttle(sp) ≈ 0.5
 
-        throttle(sp, 1)
-        delay(sp, 1)
-        @test throttle(sp) ≈ 1
-
-        throttle(sp, 0.)
-        delay(sp, 1)
+        throttle!(sp, 0.)
+        Timing.delay(sp, 0.5)
         @test throttle(sp) ≈ 0
+
+        throttle!(sp, 1)
+        Timing.delay(sp, 0.5)
+        @test throttle(sp) ≈ 1
+    end
+
+    @testset "Engine spinup test" begin
+        parts = SCH.Parts(sp.ves)
+        engine = SCH.WithTag(parts, "e1")[1] |> SCH.Engine
+        status, time_spent = ignite!(sp, engine; timeout=5)
+        @test status
+        @test 1 < time_spent < 3
+        @test SCH.Thrust(engine) > 10000
     end
 end
