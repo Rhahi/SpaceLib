54d7b19b0b4d1dbc5a5cda2873c8c65c7aa3a8a3
Rhahi <git@rhahi.com>
2022-12-07 00:36:48 +0100

    refactor(control): remove unused source setup

--- src/Control/sources.jl
+++ src/Control/sources.jl
@@ -3,17 +3,3 @@
     PARTIAL=2
     NONE=4
 end
-
-function init_autopilot(sp::Spacecraft, ref::SCR.ReferenceFrame)
-    @log_entry "Initializing autopilot"
-    control = SCH.Control(sp.ves)
-    ap = SCH.AutoPilot(sp.ves)
-    SCH.ReferenceFrame!(ap, ref)
-    if sp.system.met == 0
-        @log_trace "Autopilot initiated while mission hasn't begun"
-    end
-    if SCH.State(control).value |> ControlState ≠ FULL
-        @log_trace "Autopilot initiated while vehicle is not controllable"
-    end
-    return control, ap
-end
