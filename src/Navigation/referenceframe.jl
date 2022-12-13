18e516483f90b184aeac70716923e367f087d902
Rhahi <git@rhahi.com>
2022-12-14 02:29:41 +0900

    feat(navigation): add more reference frame interfaces

--- src/Navigation/referenceframe.jl
+++ src/Navigation/referenceframe.jl
@@ -14,9 +14,8 @@ y-axis points northwards towards the astronomical horizon (north, and tangential
 compass would point when on the surface).The z-axis points eastwards towards the astronomical horizon (east, and tangential to the
 surface of the body - east on a compass when on the surface).
 """
-function SRF(sp::Spacecraft)
-    sp.ves |> SurfaceReferenceFrame
-end
+SRF(ves::SCR.Vessel) = ves |> SCH.SurfaceReferenceFrame
+SRF(sp::Spacecraft) = SRF(sp.ves)
 
 """
 The reference frame that is fixed relative to the vessel, and orientated with the velocity vector of the vessel relative to the surface
@@ -24,9 +23,8 @@ of the body being orbited. The origin is at the center of mass of the vessel.The
 y-axis points in the direction of the vessel's velocity vector, relative to the surface of the body being orbited.The z-axis is in the
 plane of the astronomical horizon.The x-axis is orthogonal to the other two axes.
 """
-function SVRF(sp::Spacecraft)
-    sp.ves |> SurfaceVelocityReferenceFrame
-end
+SVRF(ves::SCR.Vessel) = ves |> SCH.SurfaceVelocityReferenceFrame
+SVRF(sp::Spacecraft) = SVRF(sp.ves)
 
 """
 The reference frame that is fixed relative to this celestial body, and
@@ -36,14 +34,14 @@ arbitrary direction through the equator.The y-axis points from the center of
 the body towards the north pole.The z-axis points in an arbitrary direction
 through the equator.
 """
-function BCI(sp::Spacecraft)
-    sp.ves |> SCH.Orbit |> SCH.Body |> SCH.NonRotatingReferenceFrame
-end
+BCI(body::SCR.CelestialBody) = SCH.NonRotatingReferenceFrame(body)
+BCI(ves::SCR.Vessel) = ves |> SCH.Orbit |> SCH.Body |> SCH.NonRotatingReferenceFrame
+BCI(sp::Spacecraft) = BCI(sp.ves)
 
 """Body centered body focused frame in current body"""
-function BCBF(sp::Spacecraft)
-    sp.ves |> SCH.Orbit |> SCH.Body |> SCH.ReferenceFrame
-end
+BCBF(body::SCR.CelestialBody) = SCH.ReferenceFrame(body)
+BCBF(ves::SCR.Vessel) = ves |> SCH.Orbit |> SCH.Body |> SCH.ReferenceFrame
+BCBF(sp::Spacecraft) = BCBF(sp.ves)
 
 """
 The reference frame that is fixed relative to the vessel, and orientated with
@@ -53,12 +51,10 @@ prograde/normal/radial directions.The x-axis points in the orbital anti-radial
 direction.The y-axis points in the orbital prograde direction.The z-axis points
 in the orbital normal direction.
 """
-function ORF(sp::Spacecraft)
-    sp.ves |> SCH.OrbitalReferenceFrame
-end
+ORF(ves::SCR.Vessel) = ves |> SCH.OrbitalReferenceFrame
+ORF(sp::Spacecraft) = ORF(sp.ves)
 
-function COMF(part::SCR.Part)
-    part |> SCH.CenterOfMassReferenceFrame
-end
+"Center of mass frame."
+COMF(part::SCR.Part) = part |> SCH.CenterOfMassReferenceFrame
 
 end
