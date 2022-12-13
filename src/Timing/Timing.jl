5389ffd0ac6feee34c69051b254fa74a7fcdf654
Rhahi <git@rhahi.com>
2022-12-14 02:15:06 +0900

    style(timing): rename on_time -> delay

--- src/Timing/Timing.jl
+++ src/Timing/Timing.jl
@@ -9,7 +9,7 @@ import KRPC.Interface.SpaceCenter.RemoteTypes as SCR
 import KRPC.Interface.SpaceCenter.Helpers as SCH
 
 include("utstream.jl")
-include("on_time.jl")
+include("delay.jl")
 
 export delay
 export wait_for_true, wait_for_value
